4a877dc913f7c941fe5d951e6786ecb374bb12dd
Ludovic Dumas <ludoloops@pm.me>
2023-09-20 20:11:18 -0400

    earth with stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css"
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
 import styled from "styled-components"
+import { Earth } from "./components/earth"
 
 const CanvasContainer = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
 `
 
 function App() {
-  return <CanvasContainer>hello</CanvasContainer>
+  return (
+    <CanvasContainer>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Earth />
+        </Suspense>
+      </Canvas>
+    </CanvasContainer>
+  )
 }
 
 export default App
